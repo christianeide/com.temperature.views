770294c26b359ac74b5928180507d500071c8cb8
Christian Gogstad Eide <christian.gogstad.eide@nrk.no>
2025-03-10 10:32:11 +0100

    change format to return color

--- widgets/temperature-viewer/public/card.mjs
+++ widgets/temperature-viewer/public/card.mjs
@@ -5,24 +5,32 @@ const html = htm.bind(createElement);
 
 function getTemperatureColor(value, min, max) {
   if (value === null || value === undefined)
-    return 'var(--homey-text-color-light)';
+    return { color: 'var(--homey-text-color-light)' };
 
-  if (value < min) return 'var(--homey-color-blue-500)';
-  if (max && value > max) return 'var(--homey-color-red-500)';
+  if (value < min) return { color: 'var(--homey-color-blue-500)' };
+  if (max && value > max) return { color: 'var(--homey-color-red-500)' };
 
-  return 'var(--homey-text-color)';
+  return { color: 'var(--homey-text-color)' };
 }
 
-export function Card({ children, value, min, max }) {
+export function Card({ children, value, min, max, customColorScale }) {
   const valToShow = getFormatedTemperature(value);
+
+  const { color, light } = customColorScale
+    ? customColorScale(value)
+    : getTemperatureColor(value, min, max);
 
   return html`
     <div class="temperature-card">
       <p>
         ${children}<br />
         <span
-          class="temperature-card__number"
-          style=${{ color: getTemperatureColor(value, min, max) }}
+          class="temperature-card__number ${light
+            ? 'temperature-card__number--light'
+            : ''} "
+          style=${{
+            color,
+          }}
         >
           ${valToShow ? `${valToShow}°` : '-'}
         </span>
@@ -33,14 +41,12 @@ export function Card({ children, value, min, max }) {
 
 export function SmallCard({ children, value, min, max }) {
   const valToShow = getFormatedTemperature(value);
+  const { color } = getTemperatureColor(value, min, max);
 
   return html`
     <div class="temperature-small-card">
       ${children}
-      <span
-        class="homey-text-bold"
-        style=${{ color: getTemperatureColor(value, min, max) }}
-      >
+      <span class="homey-text-bold" style=${{ color }}>
         ${valToShow ? `${valToShow}°` : '-'}
       </span>
     </div>
